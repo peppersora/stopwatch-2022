dfae4327a77817f3d7d0cbab35e23b2d0e8efa2a
peppersora <110614387+peppersora@users.noreply.github.com>
2023-03-05 22:39:59 +0900

    done!!

--- stopwatch/stopwatch.js
+++ stopwatch/stopwatch.js
@@ -1,25 +1,12 @@
-//1. 버튼 가져오기
-let startBtn = document.querySelector(".stopBox_startBtn");
-let stopBtn = document.querySelector(".stopBox_stopBtn");
-let clearBtn = document.querySelector(".stopBox_clearBtn");
-let lapBtn = document.querySelector(".stopBox_lapseBtn");
-// console.log(startBtn,stopBtn,clearBtn,lapBtn);
+let startBtn = document.querySelector(".Swa_Op-start");
+let stopBtn = document.querySelector(".Swa_Op-stop");
+let clearBtn = document.querySelector(".Swa_Op-clear");
+let lapBtn = document.querySelector(".Swa_Op-lap");
 
-/* 스톱워치 작동 방식
-strat 버튼, stop 버튼, clear버튼이 존재
-1. 60초가 되면 00이되면서 분이 01분으로 바뀌고
-60분이 되면 01시가 된다는 것을 이용...
-
-*/
 let hours = 0;
 let minutes = 0;
 let seconds = 0;
 
-/* setInterval의 중복 해결을위해
-active = false;로 설정 해놓고 Start 버튼을 클릭 할 때 
-if문으로 조건 확인 후 실행  
-false일 때, 타이머 실행 및 false -> true
--> true일 때, 현재 타이머 실행 중으로 Start 버튼을 눌러도 호출 X*/
 let active = false;
 
 startBtn.onclick = function () {
@@ -40,9 +27,6 @@ startBtn.onclick = function () {
         }
       }
 
-      /* 나타내기
-          삼항연산자 이용=> hours, minutes,seconds가 10보다 
-          작다면..? 01:...*/
       document.getElementById("time").innerText =
         (hours < 10 ? "0" + hours : hours) +
         ":" +
@@ -68,22 +52,37 @@ clearBtn.onclick = function () {
   document.getElementById("time").innerText = "00:00:00";
 };
 
-/* laptop에 기록하기 => 멈추지 않고 누르는 순간에 기록 가져오기
- */
-// 기록장을 공백으로 초기화
-// 표기법 : "lap"+i+":"+"";
 let records = "";
+var count = 0;
 
 lapBtn.onclick = function () {
+  // 숫자 증가시키키
+  let number = document.createElement("number");
+  count = count + 1;
+  number.innerText = count;
+
+  // 시간부분
   records = document.getElementById("time").innerText + "\n";
+  let list = document.querySelector("#RecordBox");
 
-  //    document.querySelector(".Record_ul").innerText=records;
-  let list = document.querySelector(".Record_ul");
+  let div = document.createElement("div");
+  list.appendChild(div);
+  // div 박스 꾸미기
 
-  let li = document.createElement("li");
-  list.appendChild(li);
+  div.style.height = "60px";
+  div.style.lineHeight = "60px";
+  div.style.boxShadow =
+    " 4px 4px 6px -1px rgba(0, 0, 0, 0.2), -4px -4px 6px -1px rgba(255, 255, 255, 1)";
+  div.style.borderRadius = "10px";
+  div.style.fontSize = "18px";
+  div.style.fontWeight = "700";
+  div.style.marginBottom = "20px";
+
   let span = document.createElement("span");
-  span.innerText = records;
+  div.style.textAlign = "center";
 
-  li.appendChild(span);
+  span.innerText =
+    count + "Lap" + "\u00A0" + "\u00A0" + "\u00A0" + "\u00A0" + records;
+
+  div.appendChild(span);
 };
